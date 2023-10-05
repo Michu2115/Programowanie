e895226eef708efe2db6d695c3a695782037723a
Michu2115 <michalstasik15@gmail.com>
2023-10-05 11:43:59 +0200

    komentarz

--- ciasteczko.php
+++ ciasteczko.php
@@ -1,34 +1,18 @@
 <?php
+$cookie_name = "promocja";
+$cookie_value = "wycieczka";
 
-var dataWygasniecia = new Date();
-dataWygasniecia.setDate(dataWygasniecia.getDate() + 2);
 
-document.cookie = "promocja=wycieczka; expires=" + dataWygasniecia.toUTCString();
+setcookie($cookie_name, $cookie_value, time()+ (48*60*60));
 
-function sprawdzCiasteczko(nazwa) {
-    var ciasteczka = document.cookie.split(';');
-    for (var i = 0; i < ciasteczka.length; i++) {
-        var ciasteczko = ciasteczka[i];
-        while (ciasteczko.charAt(0) == ' ') {
-            ciasteczko = ciasteczko.substring(1);
-        }
-        if (ciasteczko.indexOf(nazwa + '=') == 0) {
-            return true;
-        }
-    }
-    return false;
-}
 
-var czyIstnieje = sprawdzCiasteczko("promocja");
-console.log("Czy ciasteczko istnieje? " + czyIstnieje);
+if(!isset($_COOKIE[$cookie_name])){
+    echo "Cookie named '".$cookie_name ."' is not set!";
+} else {
+    echo "Coookie: '".$cookie_name."' is set! <br>";
+    echo "Value is: ".$_COOKIE[$cookie_name];
+};
 
-function usunCiasteczko(nazwa) {
-    document.cookie = nazwa + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
 
-usunCiasteczko("promocja");
-
-czyIstnieje = sprawdzCiasteczko("promocja");
-console.log("Czy ciasteczko istnieje po usunięciu? " + czyIstnieje);
-
+// setcookie($cookie_name, $cookie_value, time() - 3600); // po usunięciu haszowania, cookie zostanie usunięty
 ?>
